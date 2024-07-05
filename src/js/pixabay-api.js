440c215f6d68f54cd74761975bdfd065b658aa4e
Qvantt <65224747+Qvantt@users.noreply.github.com>
2024-07-05 12:59:40 +0300

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,10 +1,15 @@
 const API_KEY = '44792549-8b3a4a2cfb17648b3cdad98bf';
 const BASE_URL = 'https://pixabay.com/api/';
 
-export async function fetchImages(query) {
-  const response = await fetch(
+export function fetchImages(query) {
+  return fetch(
     `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`
-  );
-  const data = await response.json();
-  return data.hits;
+  )
+    .then(response => {
+      if (response.status !== 200) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    })
+    .then(data => data.hits);
 }
